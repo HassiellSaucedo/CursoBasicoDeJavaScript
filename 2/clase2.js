a060206dc4006e46276c6f50cb644bba609c9f7c
HiImChobo <matzukhe@gmail.com>
2021-01-13 20:53:39 -0700

    Learning types of variables

---
+++ 2/clase2.js
@@ -0,0 +1,15 @@
+var nombre = "Hassiell";
+
+//declarar
+var edad;
+//inicializar
+edad = 20;
+
+edad;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+  nombre: "Hassiell",
+  edad: 20,
+};
